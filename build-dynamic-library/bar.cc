81d38d8dcdb66974508699fe2e106c6baf8baa3f
Pham Van Lam <lampv606@gmail.com>
2017-02-17 09:15:46 +0700

    Create bar.cc

---
+++ build-dynamic-library/bar.cc
@@ -0,0 +1,25 @@
+#include "bar.h"
+#include <iostream>
+
+using namespace std;
+
+Bar::Bar()
+{
+	cout << "	Bar created [Bar.cc - Bar::Bar()]" << endl; 
+}
+
+Bar::~Bar()
+{
+	cout << "	Bar destroyed [Bar.cc - Bar::~Bar()]" << endl; 
+}
+
+void Bar::test()
+{
+	cout << "	Bar tested [Bar.cc - Bar::test()]" << endl;
+}
+
+Bar* BarFactory::getInstance()
+{
+	cout << "	Making Bar [Bar.cc BarFactory::getInstance()]" << endl;  
+	return new Bar; 
+}
