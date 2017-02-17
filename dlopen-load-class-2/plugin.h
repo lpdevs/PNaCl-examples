b6651ecd96d477d0a4e604caf6aee3d67569cada
Pham Van Lam <lampv606@gmail.com>
2017-02-17 09:22:05 +0700

    Create plugin.h

---
+++ dlopen-load-class-2/plugin.h
@@ -0,0 +1,19 @@
+#ifndef _PLUGIN_H_
+#define _PLUGIN_H_
+
+class Plugin
+{
+    public:
+        // default constructor
+        // virtual void test() { };
+        virtual ~Plugin() {};
+};
+
+class PluginFactory
+{
+    public:
+        virtual Plugin * getInstance() = 0;
+};
+
+#endif
+
